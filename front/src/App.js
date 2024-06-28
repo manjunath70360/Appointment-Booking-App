64ad36ef679a1bd9b46dcb116b351fcbc0877f0d
Manjunath <122592837+manjunath70360@users.noreply.github.com>
2024-06-28 17:02:21 +0530

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import Booking from"./components/calender"
 import Home from './components/home'
 import Contact from './components/contact'
 import NotFound from './components/notfound'
+import ProtectedRoute from './components/protectedRoute'
 import "./App.css"
 
 const App = ()=> {
@@ -12,9 +13,9 @@ return(
       <BrowserRouter>
       <Switch>
           <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/appointment" component={Booking} />
+          <ProtectedRoute exact path="/home" component={Home} />
+          <ProtectedRoute exact path="/contact" component={Contact} />
+          <ProtectedRoute exact path="/appointment" component={Booking} />
           <Route path="/not-found" component={NotFound} />
           <Redirect to="not-found" />
       </Switch>
